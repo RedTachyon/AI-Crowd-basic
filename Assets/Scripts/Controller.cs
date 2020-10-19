54f68207d81d0e41103806e661c051687ca2141f
Ariel Kwiatkowski <ariel.j.kwiatkowski@gmail.com>
2020-10-19 09:11:48 +0200

    Sync between lab and home

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -28,8 +28,7 @@ public class Controller : Agent
     public override void OnEpisodeBegin()
     {
         // TODO: remove position observations, use only raycasts instead
-
-        Vector3 startPos = new Vector3(-8.5f, 0.5f, 0f);
+        Vector3 startPos = new Vector3(-9f, 0.25f, UnityEngine.Random.Range(-9f, 9f));
         transform.localPosition = startPos;
         transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
 
@@ -38,28 +37,27 @@ public class Controller : Agent
 
         _previousPosition = startPos;
 
-        // goal.localPosition = new Vector3(
-        //     UnityEngine.Random.Range(-6f, 0f),
-        //     0.5f,
-        //     UnityEngine.Random.Range(-3f, 3f)
-        // );
+
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
         // TODO: change to discrete actions?
         // Forward velocity
-        Vector3 velocity = transform.forward * vectorAction[0] * moveSpeed;
-        Debug.Log(vectorAction[0]);
+        var linearSpeed = Mathf.Clamp(vectorAction[0], -0.3f, 1.0f);
+        var angularSpeed = Mathf.Clamp(vectorAction[1], -1f, 1f);
+        
+        Vector3 velocity = transform.forward * linearSpeed * moveSpeed;
+        // Debug.Log(vectorAction[0]);
         // _rigidbody.velocity = velocity;
         _rigidbody.AddForce(velocity);
 
         // Rotation
-        var rotateDir = transform.up * vectorAction[1];
+        // var rotateDir = transform.up * vectorAction[1];
         // transform.Rotate(rotateDir, Time.fixedDeltaTime * rotationSpeed);
         
-        // Vector3 rotation = transform.rotation.eulerAngles + Vector3.up * vectorAction[1] * rotationSpeed;
-        // _rigidbody.MoveRotation(Quaternion.Euler(rotation));
+        Vector3 rotation = transform.rotation.eulerAngles + Vector3.up * angularSpeed * rotationSpeed;
+        _rigidbody.MoveRotation(Quaternion.Euler(rotation));
     }
 
     public override void Heuristic(float[] actionsOut)
@@ -88,7 +86,7 @@ public class Controller : Agent
         
         sensor.AddObservation(position.x / 20f);
         sensor.AddObservation(position.z / 20f);
-        // sensor.AddObservation(rotation.y);
+        sensor.AddObservation(rotation.y / 360f);
         // sensor.AddObservation(velocity.x);
         // sensor.AddObservation(velocity.z);
         // sensor.AddObservation(angularVelocity);
@@ -99,15 +97,16 @@ public class Controller : Agent
         // Debug.Log($"Velocity {velocity}");
         // Debug.Log($"Angular {angularVelocity}");
         // Debug.Log($"Goal {goalPosition}");
-        // TODO: GetReward() apparently can be used
+        // Debug.Log($"Reward {GetCumulativeReward()}");
         
         // Compute the distance-based reward
         var prevDistance = Vector3.Distance(_previousPosition, goalPosition);
         var currentDistance = Vector3.Distance(position, goalPosition);
         var diff = prevDistance - currentDistance;
-        AddReward(0.1f * diff);  // Add reward for getting closer to the goal
+        AddReward(2f * diff);  // Add reward for getting closer to the goal
+        AddReward(-1e-2f);  // Small penalty at each step
         // Debug.Log($"Distance {currentDistance}");
-        Debug.Log($"Distance difference {diff}");
+        // Debug.Log($"Distance difference {diff}");
 
         _previousPosition = position;
     }
@@ -117,18 +116,18 @@ public class Controller : Agent
     {
         if (other.CompareTag("Goal"))
         {
-            AddReward(1f);
+            AddReward(2f);
             Debug.Log("Got the goal!");
             EndEpisode();
         }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionStay(Collision other)
     {
-        if (other.collider.CompareTag("Obstacle"))
+        if (other.collider.CompareTag("Obstacle") || other.collider.CompareTag("Agent"))
         {
-            AddReward(-0.1f);
-            // Debug.Log("Collision!");
+            AddReward(-0.5f);
+            Debug.Log($"Collision with an {other.collider.tag}!");
         }
     }
 
